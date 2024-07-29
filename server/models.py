fe7de35ca7af137e38af773b8135492bf7dceb2c
Benhin Mwendwa <benhinmwendwa7@gmail.com>
2024-07-29 12:33:31 +0300

    booking

--- server/models.py
+++ server/models.py
@@ -28,7 +28,12 @@
 
 #Bookings
 
-
+class Booking(db.Model,SerializerMixin):
+    __tablename__ = 'bookings'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    space_id = db.Column(db.Integer, db.ForeignKey('spaces.id'))
+    booking_id = db.Column(db.Integer,
 
 
 
