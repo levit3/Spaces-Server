10c516024b34c26bbb8d7d377b6108ea8357e327
Favor Vincent <vfavorm77@gmail.com>
2024-07-29 12:33:48 +0300

    spaces table created

--- server/models.py
+++ server/models.py
@@ -1,3 +1,4 @@
+from sqlalchemy_serializer import SerializerMixin
 ##Users
 
 
@@ -14,8 +15,22 @@
 
 
 ##Spaces
+class Spaces(db.Model, SerializerMixin):
+    __tablename__ ='spaces'
 
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    location = db.Column(db.String, nullable=False)
+    price_per_hour = db.Column(db.Float, nullable=False)
+    status = db.Column(db.String, nullable=False)
+    image_url = db.Column(db.String, nullable=False)
+    tenant_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    tenant = db.relationship('User', back_populates = 'spaces')
 
+    def __repr__(self):
+        return f'<Space {self.title}, {self.description}>'
+ 
 
 
 
