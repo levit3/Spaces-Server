2c73156007bf67cbd1b5d9f4e8cd8976e5e468e5
Favor Vincent <vfavorm77@gmail.com>
2024-07-29 15:44:41 +0300

    space endpoints added

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from flask import request, make_response
+from flask import request, make_response, jsonify
 from flask_restful import Resource
 
 from config import app, db, api
@@ -31,8 +31,58 @@ class User(Resource):
 
 api.add_resource(User, '/api/users/<int:user_id>')
 
+class Spaces(Resource):
+    def get(self):
+      spaces = Space.query.all()
+      space_data = [space.to_dict() for space in spaces]
+      return make_response(jsonify(space_data), 200)
+api.add_resource(Spaces, '/api/spaces>')
 
+class Space(Resource):
+    def get(self, space_id):
+        space = Space.query.get(space_id)
+        return [space.to_dict()]
 
+    def post(self):
+        data = request.get_json()
+        title = data.get('username')
+        description = data.get('email')
+        location = data.get('password')
+        price_per_hour = data.get('balance')
+        status = data.get('status')
+        
+        space = Space(
+          title=title,
+          description=description,
+          location=location,
+          price_per_hour=price_per_hour,
+          status=status,
+        )
+        db.session.add(space)
+        db.session.commit()
+        return space.to_dict()
 
+    def put(self, space_id):
+        space = Space.query.get(space_id)
+        data = request.get_json()
+        for key, value in data.items():
+            setattr(space, key, value)
+        db.session.commit()
+        return space.to_dict()
 
-  
+    def delete(self, space_id):
+       space = Space.query.get(space_id)
+       db.session.delete(space)
+       db.session.commit()
+       return space.to_dict()
+
+    def patch(self, space_id):
+     space = Space.query.get(space_id)
+     data = request.get_json()
+     for key, value in data.items():
+        setattr(space, key, value)
+     db.session.commit()
+     return space.to_dict()
+
+api.add_resource(Spaces, '/api/spaces/<int:space_id>')
+
