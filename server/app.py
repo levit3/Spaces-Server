cb7ade8d7c7b5a2dadb068d25227bbe36fd850cd
Benhin Mwendwa <benhinmwendwa7@gmail.com>
2024-07-30 09:21:45 +0300

    jwt

--- server/app.py
+++ server/app.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 from flask import request, make_response, session
-from flask_restful import Resource
+from flask_restful import Resource 
 
 from config import app, db, api
 from models import User, Review, Space, Payment ,Booking
+
 
 
 app.route('/')
